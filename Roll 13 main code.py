ad8fcc5ae34913bb4372c153a85a1265d7a97c07
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2024-02-08 12:23:12 +1300

    Add files via upload

--- Roll 13 main code.py
+++ Roll 13 main code.py
@@ -15,9 +15,22 @@ def statement_generator(text, decoration):
 def instructions():
     statement_generator("Instructions/information", "-")
     print()
-    print("Please choose a number than is greater than or equal to one and less than or equal to 200")
+    print("At the start of each round, the user and the computer each roll two dice."
+          "The initial number of points for each player is the total shown by the dice.Then, taking turns, "
+          "the user and computer each roll a single die and add the result to their points.  ")
+    print("The goal is to get 13 points (or slightly less) for a given round.  "
+          "Once you are happy with your number of points, you can ‘pass’.")
     print()
-    print("Complete as many calculations as necessary, pressing <enter> at the end of each "
+    print("If you go over 13, then you lose the round (and get zero points). If the computer goes over 13, "
+          "the round ends and your score is the number of points that you have earned.")
+    print("If the computer gets more points than you (eg: you get 10 and they get 11, then you lose your score stays "
+          "the same).")
+    print("If you get more points than the computer (but less than 14 points), you win and add your points to your "
+          "score. The computer’s score stays the same.")
+    print("The first roll of your dice is a double, then your score is increased by double the number of points, "
+          "provided you win.  If the computer’s first roll of the dice is a double, "
+          "then its points are not doubled (this gives the human player a slight advantage).")
+    print("The ultimate winner of the game is the first one to get to the specified score goal."
           "calculation or any key to quit.")
     print()
     return ""
@@ -26,10 +39,15 @@ def instructions():
 # Main routine goes here
 
 # Heading
-statement_generator("Factors Calculator", "-")
+statement_generator("Roll13", "🎲")
 
 # Displays instructions if user has not used the program before
-first_time = input("Press <enter> to see the instructions or any key to continue")
+first_time = input("Would you like to see the instructions? ").lower()
 
-if first_time == "":
+if first_time == "yes" or first_time == "y":
+    print("You chose yes")
     instructions()
+elif first_time == "no" or first_time == "n":
+    print("You chose no")
+else:
+    print("Please answer yes or no ")
